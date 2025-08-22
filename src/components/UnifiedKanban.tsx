168cc4c0a5cfab308d0d062f26b7fd52575618d1
BennersTaga <s-taga@benners.co.jp>
2025-08-22 13:08:01 +0900

    Create UnifiedKanban.tsx

---
+++ src/components/UnifiedKanban.tsx
@@ -0,0 +1,8 @@
+// src/components/UnifiedKanban.tsx
+export default function UnifiedKanbanPrototypeV2() {
+  return (
+    <div style={{ padding: 24 }}>
+      Unified Kanban placeholder
+    </div>
+  );
+}
